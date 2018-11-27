647486314c6566df7b5403928828990abf592661
June YAMAMOTO <yamamoto@questetra.com>
2018-11-27 17:09:55 +0900

    固定サービスアイコンを追加 Noto Sans を Google fonts に移行

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,18 +3,17 @@ module.exports = function(grunt) {
         uglify: {
             dist: {
                 files: [
-                    {'dist/js/main-min.js': 'src/js/main.js'},
-                    {'dist/js/icons-min.js': 'src/js/icons.js'}
+                    {'docs/js/main-min.js': 'src/js/main.js'},
+                    {'docs/js/icons-min.js': 'src/js/icons.js'},
+                    {'docs/js/static-icons.js': 'src/js/static-icons.js'}
                 ]
             }
         },
         copy: {
             fonts: {
                 files: [
-                    { expand: true, cwd: 'src/font/Questetra-Icon-Font-A/fonts/', src: ['*.*'], dest: 'dist/fonts/', filter: 'isFile' },
-                    { expand: true, cwd: 'src/font/Noto/', src: ['*.*'], dest: 'dist/fonts/', filter: 'isFile' },
-                    { expand: true, cwd: 'src/font/Questetra-Icon-Font-A/', src: ['style.css'], dest: 'dist/', filter: 'isFile' },
-                    { expand: true, cwd: 'src/font/Noto/', src: ['*.css'], dest: 'dist/', filter: 'isFile' }
+                    { expand: true, cwd: 'src/font/Questetra-Icon-Font-A/fonts/', src: ['*.*'], dest: 'docs/fonts/', filter: 'isFile' },
+                    { expand: true, cwd: 'src/font/Questetra-Icon-Font-A/', src: ['style.css'], dest: 'docs/', filter: 'isFile' }
                 ]
             }
         }
@@ -31,5 +30,5 @@ module.exports = function(grunt) {
     */
 
     grunt.registerTask('_Dist', ['copy', 'uglify']);
-    
+
 };
