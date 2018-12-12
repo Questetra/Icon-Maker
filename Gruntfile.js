954f1ce32ae81cf6a3bb78c7b00aaa4f5b7cc19b
nishi99 <nishi@questetra.com>
2018-12-12 17:43:04 +0900

    grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -20,7 +20,7 @@ module.exports = function(grunt) {
     });
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-copy');
-    grunt.loadNpmTasks("grunt-contrib-less");
+    //grunt.loadNpmTasks("grunt-contrib-less");
     /*
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks("grunt-contrib-less");
@@ -29,6 +29,6 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks("grunt-contrib-compress");
     */
 
-    grunt.registerTask('_Dist', ['copy', 'uglify']);
+    grunt.registerTask('default', ['copy', 'uglify']);
 
 };
